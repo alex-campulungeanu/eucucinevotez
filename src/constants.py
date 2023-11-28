c5859f9d39e32293699f0fcbb9193afecf361342
Alex Campulungeanu <allecs1987@yahoo.com>
2023-11-28 14:49:43 +0200

    add black formatter

--- src/constants.py
+++ src/constants.py
@@ -5,10 +5,10 @@ load_dotenv()
 
 ROOT_DIR = os.path.abspath(os.curdir)
 
-MAX_RETRYS_REQUEST = 80
+MAX_RETRYS_REQUEST = 150
 JIRA_SESSION_ID = os.getenv('JSESSIONID')
 BASE_URL = os.getenv('BASE_URL')
 
 LAST_VOTE_KEY_STORAGE = 'eucucinevotez.last_vote'
 WAIT_SECONDS = 2
-NR_OF_DEVELOPERS = 10
+NR_OF_DEVELOPERS = os.getenv('NR_OF_DEVELOPERS', 10)
